aa18f30a899d86ce06ad6463847a79e232158af5
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-03-18 21:41:35 +0800

    java

---
+++ src/main/java/com/ocp/day02/Student.java
@@ -0,0 +1,28 @@
+package com.ocp.day02;
+
+public class Student {
+    String name;
+    int age;
+    BMI bmi;
+    Exam exam;
+
+    //設計建構子
+    Student(String name, int age, double height, double weight) {
+        this.name = name;
+        this.age = age;
+        this.bmi = new BMI(name, height, weight);
+        
+    }
+    
+    
+    void print(){
+        System.out.printf("姓名:%s 年齡:%d\n", name ,age);
+        bmi.print();
+        if(exam == null) {
+            System.err.println("此學生無成績");
+        } else {
+            System.err.println(exam.getPrintData());
+        }
+    
+    }
+}
