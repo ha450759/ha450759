f8b0095c0a44cec329ba7edaffdc675a3d68266d
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-03-23 20:01:35 +0800

    java

---
+++ src/main/java/com/ocp/day04/bar/Money.java
@@ -0,0 +1,6 @@
+package com.ocp.day04.bar;
+
+public class Money {
+    public int amount = 10000;
+    
+}
---
+++ src/main/java/com/ocp/day04/foo/Money.java
@@ -0,0 +1,6 @@
+package com.ocp.day04.foo;
+
+public class Money {
+    public  int amount = 200_0000;
+    
+}
