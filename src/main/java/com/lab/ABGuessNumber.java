16af161686afadedc3a0709ee8407f2bca1edd40
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-03-30 21:40:02 +0800

    java

---
+++ src/main/java/com/lab/ABGuessNumber.java
@@ -0,0 +1,39 @@
+  
+package com.lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ABGuessNumber {
+    public static void main(String[] args) {
+        int[] ansArray = Util.getAnsArray();//{5, 8, 4, 1};
+        System.out.println(Arrays.toString(ansArray));
+        do {            
+            Scanner sc = new Scanner(System.in);
+            System.out.print("請輸入一組不重複的四位數: ");
+            String guess = sc.next();
+            int[] guessArray = {guess.charAt(0)-48, guess.charAt(1)-48, guess.charAt(2)-48, guess.charAt(3)-48};
+            // 判斷 a, b
+            int a = 0, b = 0;
+            for(int i=0;i<4;i++) {
+                if(ansArray[i] == guessArray[i]) {
+                    a++;
+                }
+            }
+            for(int i=0;i<4;i++) {
+                for(int k=0;k<4;k++) {
+                    if(ansArray[i] == guessArray[k]) {
+                        b++;
+                        break;
+                    }
+                }
+            }
+            b -= a; //b = b - a;
+            System.out.printf("%d A %d B\n", a, b);
+            if(a == 4) {
+                System.out.println("Bingo");
+                break;
+            }
+        } while (true);
+    }
+}
---
+++ src/main/java/com/ocp/day07/StringEquals2.java
@@ -0,0 +1,18 @@
+package com.ocp.day07;
+
+public class StringEquals2 {
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = "Java";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        //將 s1 放入String pool
+        s1 = s1.intern();
+        System.out.println(s1 == s2);
+        String s3 = "jAVA";
+        System.out.println(s2.equals(s3));// 有分大小寫的比較
+        System.out.println(s2.equalsIgnoreCase(s3));// 不分大小寫的比較
+        
+        
+    }
+}
