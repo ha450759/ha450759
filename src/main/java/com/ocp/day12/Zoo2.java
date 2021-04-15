b51c397645000fbdb7025245b00f0ee4ce354f71
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-04-15 20:33:11 +0800

    java

---
+++ src/main/java/com/ocp/day12/Zoo2.java
@@ -0,0 +1,15 @@
+package com.ocp.day12;
+
+public class Zoo2 {
+    public static void main(String[] args) {
+        Cat cat = new  Cat();
+        playShout(cat);
+        Tiger tiger = new Tiger();
+        playShout(tiger);
+        Cat cat2 = new Tiger();
+        playShout(cat2);
+    }
+    public static void playShout(Cat x) {
+        x.shout();
+    }
+}
