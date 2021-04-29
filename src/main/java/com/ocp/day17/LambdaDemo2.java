1de094fd71c4fd8a3c1502d4da4e94877988e3af
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-04-29 21:42:41 +0800

    java

---
+++ src/main/java/com/ocp/day17/LambdaDemo2.java
@@ -0,0 +1,20 @@
+package com.ocp.day17;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.stream.Stream;
+
+public class LambdaDemo2 {
+    public static void main(String[] args) {
+        // 求此人總分與平均 = ?
+        DoubleSummaryStatistics stat =Stream.of("國文", 100, "英文", 90.5, "數學", 70.5, "座號", 7)
+                .filter(x -> x instanceof Double)
+                .mapToDouble(x -> (double)x) // 物件 Double 轉基本 double 才會變成 DoubleStream
+                .summaryStatistics();
+        
+        
+        System.out.println(stat.getSum());
+        System.out.println(stat.getAverage());
+        
+        
+    }
+}
---
+++ src/main/java/com/ocp/day18/School.java
@@ -0,0 +1,35 @@
+
+package com.ocp.day18;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.function.Consumer;
+
+public class School {
+    public static void main(String[] args) {
+        //Arrays.stream(DataCenter.getPeople())
+                //.peek(e -> System.out.println(e.getClass().getTypeName()))
+                //.forEach(System.out::println);
+        // 請問考試的總分與平均 = ?
+        IntSummaryStatistics state = Arrays.stream(DataCenter.getPeople())
+                .filter(x -> x instanceof Student)
+                .map(x -> (Student)x)
+                .mapToInt(x -> x.getScore())
+                .summaryStatistics();
+        System.out.println(state.getSum());
+        System.out.println(state.getAverage());
+        
+        // 印出不及格的學生,分數,老師與總平均
+        Consumer<Student> p = x -> System.out.printf("學生:%s, 分數:%d, 老師:%s\n", x.getName(), x.getScore(), x.getTeacher().getName());
+        double avg = Arrays.stream(DataCenter.getPeople())
+                .filter(x -> x instanceof Student)
+                .map(x -> (Student)x)
+                .filter(x -> x.getScore() < 60)
+                .peek(p)
+                .mapToInt(Student::getScore)
+                .average()
+                .getAsDouble();
+        System.out.println("不及格平均: " + avg);
+        
+    }
+}
