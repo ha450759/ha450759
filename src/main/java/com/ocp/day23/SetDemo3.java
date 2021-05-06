f94c80c47c8528fdb955029abbd2186b1d8755c0
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-05-06 21:24:35 +0800

    java

---
+++ src/main/java/com/ocp/day23/SetDemo3.java
@@ -0,0 +1,28 @@
+package com.ocp.day23;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo3 {
+    public static void main(String[] args) {
+        // 泛型<> : 限定型別
+        Set<Integer> scores = new  LinkedHashSet<>();
+        scores.add(100);
+        scores.add(90);
+        scores.add(80);
+        System.out.println(scores);
+        Iterator<Integer> iter = scores.iterator();
+        int sum = 0;
+        while (iter.hasNext()) {
+            Integer next = iter.next();
+            sum += next;
+        }
+        System.out.println(sum);
+        // Java 8
+        sum = scores.stream().mapToInt(Integer::intValue).sum();
+        //sum = scores.stream().mapToInt(score -> score.intValue()).sum();
+        //sum = scores.stream().mapToInt(score -> score).sum();
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/com/ocp/day23/SetDemo4.java
@@ -0,0 +1,26 @@
+package com.ocp.day23;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo4 {
+    public static void main(String[] args) {
+        // 國,英,數的分數是介於0~100之間
+        Set<Integer> scores = new  LinkedHashSet<>();
+        scores.add(100);
+        scores.add(200);
+        scores.add(90);
+        scores.add(-150);
+        scores.add(80);
+        System.out.println(scores);
+        // 請用 Java 8 計算國,英,數的總數
+        int sum = scores.stream()
+                .filter(Score::isValid)
+                //.filter(score -> score >=0 && score <=100)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+            
+        }
+    }
+
